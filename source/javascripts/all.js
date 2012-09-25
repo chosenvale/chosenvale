645be3791bbac23dd0d3254ff47a5cfcb17b9049
Alexander Wenzowski <alexander@wenzowski.com>
2012-09-25 16:17:01 -0400

    toggle application form using js

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -14,24 +14,37 @@
 
     // Trigger subnav affix with custom offset:
     //  - Main menu sticky on desktop but not on tablet
-    var $offset = $('.subnav').offset().top
-      , $offmin = $offset - $('.navbar').height()
-
-    $('.subnav').affix({
-      offset: {
-        top: function () { 
-          return $(window).width() > 890 ? $offmin : $offmin+$('.navbar').height()
+    var $subnav = $('.subnav')
+    if ($subnav.length) {
+      var $offset = $subnav.offset().top
+        , $offmin = $offset - $('.navbar').height()
+      $subnav.affix({
+        offset: {
+          top: function () { 
+            return $(window).width() > 890 ? $offmin : $offmin+$('.navbar').height()
+          }
         }
-      }
-    })      
-
-    // event handler for subnav scrolling
-    var offset = 102;
-    $('.subnav li a').click(function(event) {
+      })
+      // event handler for subnav scrolling
+      var offset = 102
+      $subnav.find('li a').click(function(event) {
         event.preventDefault();
         $($(this).attr('href'))[0].scrollIntoView();
         scrollBy(0, -offset);
-    });
+      });
+    }
+
+
+    $('#active-application').find('.btn').bind('click', function(event) {
+      if ( $(this).attr('id') === 'trumpet' ) {
+        $('section#trumpet-application').removeClass('hidden');
+        $('section#percussion-application').addClass('hidden');         
+      } else if ( $(this).attr('id') === 'percussion' ) {
+        $('section#trumpet-application').addClass('hidden');         
+        $('section#percussion-application').removeClass('hidden');    
+      }
+    })
+
 
   })
 
