44debdd9f242f31925da2b8bcb58f8dafb162c4d
Alexander Wenzowski <alexander@wenzowski.com>
2012-10-12 14:47:01 -0400

    only valid form data should be submitted

--- source/javascripts/contact.js
+++ source/javascripts/contact.js
@@ -14,6 +14,7 @@ $(document).ready(function(){
 
   var $contact_form = $ready.find('form');
 
+  // Declare form validation rules for jquery.validate
   $contact_form.validate({
     rules: {
       "message[from_name]": {
@@ -41,7 +42,80 @@ $(document).ready(function(){
     }
   });
 
-  // $contact_form.valid();
+  // State helpers
+  function showSendingMessage() {
+    $ready.addClass('hidden');
+    $sending.removeClass('hidden');
+    $sent.addClass('hidden');
+    $failure.addClass('hidden');
+  }
+  function showSentMessage() {
+    $ready.addClass('hidden');
+    $sending.addClass('hidden');
+    $sent.removeClass('hidden');
+    $failure.addClass('hidden');
+  }
+  function showFailureMessage() {
+    $ready.addClass('hidden');
+    $sending.addClass('hidden');
+    $sent.addClass('hidden');
+    $failure.removeClass('hidden');
+  }
+  function showContactForm() {
+    $ready.removeClass('hidden');
+    $sending.addClass('hidden');
+    $sent.addClass('hidden');
+    $failure.addClass('hidden');
+  }
+
+  // Always allow users to return to the "ready" state
+  $('button.show-contact-form').bind('click', function(event) {
+    showContactForm();
+  });
+
+  // Permit form data to be converted to json
+  $.fn.serializeFormJSON = function() {
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+      if (o[this.name] !== undefined) {
+        if (!o[this.name].push) {
+          o[this.name] = [o[this.name]];
+        }
+        o[this.name].push(this.value || '');
+      } else {
+        o[this.name] = this.value || '';
+      }
+    });
+    return o;
+  };
+
+  // Handle form submission with validation
+  $contact_form.submit(function(e) {
+    if ( $contact_form.valid() ) {
+      var $form = $(this);
+      var data = $form.serializeFormJSON();
+      var url = 'http://entio-postage.herokuapp.com/messages/send.json?callback=?';
+      showSendingMessage();
+      $.ajax({
+        type: 'POST',
+        url: url,
+        async: false,
+        jsonpCallback: 'jsonCallback',
+        contentType: "application/json",
+        dataType: 'jsonp',
+        data: data,
+        success: function(json) {
+          console.log(json);
+          showSentMessage();
+        },
+        error: function(e) {
+          console.log(e.message);
+          showFailureMessage();
+        }
+      });      
+    }
+    e.preventDefault();
+  });
   
 });
-
