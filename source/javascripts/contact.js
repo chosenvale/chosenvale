03ee8487221e91a1dfde3596a4647e03bacb7f6c
Alexander Wenzowski <alexander@wenzowski.com>
2012-10-17 20:24:17 -0400

    refactor contact form mailer (prep for #55)

--- source/javascripts/contact.js
+++ source/javascripts/contact.js
@@ -1,18 +1,11 @@
 //= require jquery
 //= require jquery.validate.js
+//= require postage
 
 $(document).ready(function(){
 
   // This section should only exist on the Contact page.
-  var $section = $('section#contact-us');
-
-  // Search children of the relevant section if it exists.
-  var $ready   = $section.find('#contact');
-  var $sending = $section.find('#sending');
-  var $sent    = $section.find('#sent');
-  var $failure = $section.find('#failure');
-
-  var $contact_form = $ready.find('form');
+  var $contact_form = $('section#contact-us div.postage form');
 
   // Declare form validation rules for jquery.validate
   $contact_form.validate({
@@ -42,80 +35,15 @@ $(document).ready(function(){
     }
   });
 
-  // State helpers
-  function showSendingMessage() {
-    $ready.addClass('hidden');
-    $sending.removeClass('hidden');
-    $sent.addClass('hidden');
-    $failure.addClass('hidden');
-  }
-  function showSentMessage() {
-    $ready.addClass('hidden');
-    $sending.addClass('hidden');
-    $sent.removeClass('hidden');
-    $failure.addClass('hidden');
-  }
-  function showFailureMessage() {
-    $ready.addClass('hidden');
-    $sending.addClass('hidden');
-    $sent.addClass('hidden');
-    $failure.removeClass('hidden');
-  }
-  function showContactForm() {
-    $ready.removeClass('hidden');
-    $sending.addClass('hidden');
-    $sent.addClass('hidden');
-    $failure.addClass('hidden');
-  }
-
-  // Always allow users to return to the "ready" state
-  $('button.show-contact-form').bind('click', function(event) {
-    showContactForm();
-  });
-
-  // Permit form data to be converted to json
-  $.fn.serializeFormJSON = function() {
-    var o = {};
-    var a = this.serializeArray();
-    $.each(a, function() {
-      if (o[this.name] !== undefined) {
-        if (!o[this.name].push) {
-          o[this.name] = [o[this.name]];
-        }
-        o[this.name].push(this.value || '');
-      } else {
-        o[this.name] = this.value || '';
-      }
-    });
-    return o;
-  };
-
-  // Handle form submission with validation
+  // Handle valid form submission
   $contact_form.submit(function(e) {
-    if ( $contact_form.valid() ) {
-      var $form = $(this);
-      var data = $form.serializeFormJSON();
-      var url = 'http://entio-postage.herokuapp.com/messages/send.json?callback=?';
-      showSendingMessage();
-      $.ajax({
-        type: 'POST',
-        url: url,
-        async: false,
-        jsonpCallback: 'jsonCallback',
-        contentType: "application/json",
-        dataType: 'jsonp',
-        data: data,
-        success: function(json) {
-          console.log(json);
-          showSentMessage();
-        },
-        error: function(e) {
-          console.log(e.message);
-          showFailureMessage();
-        }
-      });      
-    }
     e.preventDefault();
+    $contact_form.sendWithPostage();
   });
   
+  // Always allow users to return to the "ready" state
+  $('button.show-contact-form').bind('click', function(event) {
+    $(this).postageState('ready');
+  });
+
 });
---
+++ source/javascripts/postage.js
@@ -0,0 +1,71 @@
+//= require jquery
+
+$(document).ready(function(){
+
+  // Permit form data to be converted to json
+  $.fn.serializeFormJSON = function() {
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+      if (o[this.name] !== undefined) {
+        if (!o[this.name].push) {
+          o[this.name] = [o[this.name]];
+        }
+        o[this.name].push(this.value || '');
+      } else {
+        o[this.name] = this.value || '';
+      }
+    });
+    return o;
+  };
+
+  // Show correct transmission state
+  $.fn.postageState = function($state) {
+    $viewState = {}
+    $container = this.closest('.postage');
+
+    $viewState['ready']    = $container.find('.ready');
+    $viewState['sending']  = $container.find('.sending');
+    $viewState['sent']     = $container.find('.sent');
+    $viewState['failure']  = $container.find('.failure');
+
+    $.each($viewState, function(stateString, stateContainer) {
+      if (stateString == $state) {
+        // console.log('Showing .'+stateString+'.');
+        stateContainer.removeClass('hidden');
+      } else {
+        // console.log('Hiding .'+stateString+'.')        
+        stateContainer.addClass('hidden');
+      }
+    });
+  }
+
+  $.fn.sendWithPostage = function() {
+    $form = $(this);
+    if ( $form.valid() ) {
+      var data = $form.serializeFormJSON();
+      var url = 'http://entio-postage.herokuapp.com/messages/send.json?callback=?';
+      $form.postageState('sending');
+      $.ajax({
+        type: 'POST',
+        url: url,
+        async: false,
+        jsonpCallback: 'jsonCallback',
+        contentType: "application/json",
+        dataType: 'jsonp',
+        data: data,
+        success: function(json) {
+          console.log(json);
+          $form.postageState('sent');
+          // $form.find(':input').not(':button, :submit, :reset, :hidden').val('').removeAttr('checked').removeAttr('selected');
+          $form.validate().resetForm();
+        },
+        error: function(e) {
+          console.log(e.message);
+          $form.postageState('failure');
+        }
+      });      
+    }
+  }
+
+});
